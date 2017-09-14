7dc999bd9224f6de73a2d3843ab54d50e1a9ef10
munsiwoo <mun.xiwoo@gmail.com>
2017-09-14 19:28:14 +0900

    save.php

--- save.php
+++ save.php
@@ -1,28 +1,28 @@
 <?php
-	session_start();
 	error_reporting(0);
+	session_start();
 	include 'config.php';
 
 	if(!$_SESSION['login']){
 		exit("<script>location.href='./index.php';</script>");
 	}
+
 	if(isset($_POST['idx'], $_POST['contents'])){
 		$idx = (int)$_POST['idx'];
 		$contents = addslashes($_POST['contents']);
-		$row = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM `data` WHERE idx='$idx';"));
+		$chk_query = "SELECT * FROM `data` WHERE idx='$idx'";
 		$date = date("Y-m-d h:i:s A");
 		
-		if($row){ // contents edit
+		if($row = mysqli_fetch_assoc(mysqli_query($conn, $chk_query))){ // edit
 			$fdate = $row['fdate'];
 			$query = "UPDATE `data` SET ldate='$date', contents='$contents' WHERE idx='$idx'";
 		}
-		else { // new contents
+		else { // new file
 			$fdate = $date;
 			$query = "INSERT INTO `data` VALUES ('$idx', '$date', '$date', '$contents')";
 		}
 
-		mysqli_query($conn, $query);
-		mysqli_close($conn);
+		mysqli_query($conn, $query) or die('save error');
 		echo $fdate."<br>".$date;
 	}
 ?>
